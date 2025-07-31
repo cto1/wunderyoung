322aa31892e36b40a50089200f596b4ddfc04387
Naitik Solanki <solankinaitik00@gmail.com>
2025-07-31 13:58:14 +0530

    WUN-7 Done

--- public_html/app-new/js/global.js
+++ public_html/app-new/js/global.js
@@ -15,6 +15,39 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log("Modal already shown or checkbox not found, skipping...");
     }
 
+    // ----- Dropdowns Auto Closing -----
+    const dropdowns = document.querySelectorAll("details[data-auto-close]");
+
+    if (dropdowns.length > 0) {
+        document.addEventListener("click", function (e) {
+            dropdowns.forEach((details) => {
+                if (details.open && !details.contains(e.target)) {
+                    details.removeAttribute("open");
+                }
+            });
+        });
+
+        dropdowns.forEach((el) => {
+            el.addEventListener("toggle", function () {
+                if (el.open) {
+                    dropdowns.forEach((other) => {
+                        if (other !== el) other.removeAttribute("open");
+                    });
+                }
+            });
+        });
+
+        let resizeTimeout;
+        window.addEventListener("resize", () => {
+            clearTimeout(resizeTimeout);
+            resizeTimeout = setTimeout(() => {
+                dropdowns.forEach((details) => {
+                    details.removeAttribute("open");
+                });
+            }, 200);
+        });
+    }
+
 });
 
 
